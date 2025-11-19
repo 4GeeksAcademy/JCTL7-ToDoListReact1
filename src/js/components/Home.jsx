60fa56922a048be2d8f5e926c56ac4e2e3eff464
Julio Cesar <juliocesartrujillolugo@gmail.com>
2025-11-19 16:01:40 +0000

    SencilloyBasico

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,43 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+import React, { useState } from "react";
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+    const [inputValue, setInputValue] = useState("");
+    const [todos, setTodos] = useState([]);
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    return (
+        <div className="container">
+            <h1>My To Do List</h1>
+            <ul className="list-group">
+                <li className="list-group-item">
+                    <input 
+                        type="text" 
+                        className="form-control"
+                        onChange={(e) => setInputValue(e.target.value)}
+                        value={inputValue}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter") {
+                                if (inputValue.trim() !== "") { 
+                                    setTodos(todos.concat([inputValue.trim()]));
+                                    setInputValue("");
+                                }
+                            }
+                        }}	
+                        placeholder="Cosas Por Hacer"
+                    /> 
+                </li>
+                {todos.map((t, index) => (
+                    <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
+                       {t}{""}
+                        <i className="fas fa-trash-alt" 
+                            onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}
+                        ></i>
+                    </li>
+                ))}
+            </ul>
+            <div className="mt-3 text-muted">
+                {todos.length} {todos.length === 1 ? 'tarea' : 'tareas'} pendiente(s)
+            </div>
+        </div>
+    );
 };
 
 export default Home;
