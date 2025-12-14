44ace75f08471f39b875ce3ccae80a07f5605339
Julio Cesar <juliocesartrujillolugo@gmail.com>
2025-12-14 14:28:36 +0000

    Terminado

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,34 +1,89 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 const Home = () => {
     const [inputValue, setInputValue] = useState("");
     const [todos, setTodos] = useState([]);
+    useEffect(function () {
+        fetch('https://playground.4geeks.com/todo/users/JCTL7')
+            .then(resp => resp.json()
+            )
+            .then(data => {
+                setTodos(data.todos);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }, []
+    );
+
+    const saveTodos = (newTodos) => {
+        fetch('https://playground.4geeks.com/todo/todos/JCTL7', {
+            method: 'POST',
+            body: JSON.stringify({
+                label: newTodos,
+                is_done: false,
+            }
+            ),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                setTodos([...todos, data]);
+            })
+            .catch(error => {
+
+                console.log(error);
+            });
+    }
+    function deleteTodos(id) {
+        fetch(`https://playground.4geeks.com/todo/todos/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(resp => {
+                if (resp.json) {
+                    setTodos(todos.filter(t => t.id !== id));
+                }
+                return resp;
+            })
+            .catch(error => {
+                console.log(error);
+            });
+
+    }
 
     return (
         <div className="container">
             <h1>My To Do List</h1>
             <ul className="list-group">
                 <li className="list-group-item">
-                    <input 
-                        type="text" 
+                    <input
+                        type="text"
                         className="form-control"
                         onChange={(e) => setInputValue(e.target.value)}
                         value={inputValue}
                         onKeyDown={(e) => {
                             if (e.key === "Enter") {
-                                if (inputValue.trim() !== "") { 
-                                    setTodos(todos.concat([inputValue.trim()]));
+                                if (inputValue.trim() !== "") {
+                                    saveTodos(inputValue.trim());
                                     setInputValue("");
-                                }
+                                };
                             }
-                        }}	
+                        }}
                         placeholder="Cosas Por Hacer"
-                    /> 
+                    />
                 </li>
                 {todos.map((t, index) => (
                     <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>
-                       {t}{""}
-                        <i className="fas fa-trash-alt" 
-                            onClick={() => setTodos(todos.filter((t, currentIndex) => index != currentIndex))}
+                        {t.label}{""}
+                        <i
+                            className="fas fa-trash-alt"
+                            onClick={() => deleteTodos(t.id)}
+                            style={{ cursor: "pointer" }}
                         ></i>
                     </li>
                 ))}
